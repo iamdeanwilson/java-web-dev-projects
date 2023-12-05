b6303d8acb4c6b8e6010caccc15bd80151532a9b
Dean Wilson <Dean Wilson>
2023-12-04 20:12:10 -0600

    Added comments to code to make it more readable

--- control-flow-and-collections/Studio/counting-characters/src/main/java/org/example/Main.java
+++ control-flow-and-collections/Studio/counting-characters/src/main/java/org/example/Main.java
@@ -6,15 +6,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        // Code using the default string from Canvas
+        // To be used later to confirm if character in string is an alphabetical letter or not
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        // Default string from Canvas
 //        String myString = "If the product of two terms is zero then common sense says at least one of the two terms has " +
 //                "to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into " +
 //                "a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s " +
 //                "pretty straightforward from there.";
 
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-        // Code for accepting user input for string
+        // Accept user input for string
 //        Scanner input;
 //        input = new Scanner(System.in);
 //        System.out.println("Enter a string to count the characters: ");
@@ -22,29 +23,33 @@ public class Main {
 //        input.close();
 
 
-        // Code for inputting text from a file
+        // Use text from a text file
         File filePath = new File("C:/Users/iamde/Desktop/Launchcode/java-web-dev-projects/control-flow-and-collections/Studio/counting-characters/test-text-document.txt");
         Scanner input = new Scanner(filePath);
         String myString = input.nextLine();
         input.close();
 
-
+        // Convert string into array of characters
         char[] charactersInString = myString.toLowerCase().toCharArray();
 
+        // Initialize the HashMap that will store the characters and their respective counts
         HashMap<Character, Integer> characterCount = new HashMap<>();
 
+        // Initialize each letter as a key and start the count at 0
         for (char c : charactersInString) {
             if (alphabet.contains(String.valueOf(c))) {
                 characterCount.put(c, 0);
             }
         }
 
+        // Increase the count for each letter as it appears in the string
         for (char c : charactersInString) {
             if (alphabet.contains(String.valueOf(c))) {
                 characterCount.put(c, characterCount.get(c) + 1);
             }
         }
 
+        // Print results to console
         for (Map.Entry<Character, Integer> characterCountPrint : characterCount.entrySet()) {
             System.out.println(characterCountPrint.getKey() + ": " + characterCountPrint.getValue());
         }
