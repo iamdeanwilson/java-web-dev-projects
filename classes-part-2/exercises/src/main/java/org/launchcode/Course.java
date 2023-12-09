9e17b0e47c264227ff1d6782c68201b48a136e68
Dean Wilson <Dean Wilson>
2023-12-09 14:42:05 -0600

    Completed Chapter 5 Exercises

--- classes-part-2/exercises/src/main/java/org/launchcode/Course.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Course.java
@@ -11,7 +11,33 @@ public class Course {
     //  just the class fields.
 
 
+    public String toString() {
+
+        String courseDetails = "The " + this.topic + " class is taught by "
+                + this.instructor + " and the enrolled students are: " + this.enrolledStudents;
+        return courseDetails;
+    }
+
+
+
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
+
+    public boolean equals(Object toBeCompared) {
+        if (toBeCompared == this) {
+            return true;
+        }
+
+        if (toBeCompared == null) {
+            return false;
+        }
+
+        if (toBeCompared.getClass() != getClass()) {
+            return false;
+        }
+
+        Course theCourse = (Course) toBeCompared;
+        return theCourse.getTopic() == getTopic();
+    }
 
 }
