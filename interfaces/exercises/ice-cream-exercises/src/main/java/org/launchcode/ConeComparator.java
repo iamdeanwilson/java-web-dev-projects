f5c3b330eeb1c35133e776a32df1685c5dd0fc8d
Dean Wilson <Dean Wilson>
2024-01-01 18:33:42 -0600

    Completed Interfaces Exercises

---
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/ConeComparator.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import java.util.Comparator;
+
+public class ConeComparator implements Comparator<Cone> {
+
+    @Override
+    public int compare(Cone o1, Cone o2) {
+        return o1.getCost().compareTo(o2.getCost());
+    }
+
+}
