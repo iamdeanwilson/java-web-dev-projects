0f76f0b104c4e961618e3f0a333d1a6d557b679c
Dean Wilson <Dean Wilson>
2024-01-09 19:10:30 -0600

    Completed Exceptions Exercises

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -2,9 +2,13 @@ package org.launchcode;
 
 import java.util.HashMap;
 
+import static java.util.Objects.isNull;
+
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+
+//        Divide(7, 0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -12,16 +16,35 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
+        System.out.println(CheckFileExtension(""));
         // Test out your CheckFileExtension() function!
     }
 
     public static void Divide(int x, int y)
     {
-        // Write code here!
+
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Cannot divide by zero!");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
+    public static int CheckFileExtension(String fileName) {
         // Write code here!
-    }
+        int score = 0;
+        if (fileName.contains(".java")) {
+            score = 1;
+        }
+        if (!fileName.contains(".java")) {
+            score = 0;
+        }
+        if (isNull(fileName)) {
+            score = -1;
+        }
+            return score;
+        }
+
 }
