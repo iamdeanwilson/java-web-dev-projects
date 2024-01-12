4a7ec1c21aefcbadbb05941ad57297f0dfdd01c0
Dean Wilson <Dean Wilson>
2024-01-12 10:17:14 -0600

    working on Thymeleaf

--- controllers-and-routing/exercises/controllers-routing-exercises/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ controllers-and-routing/exercises/controllers-routing-exercises/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,86 +1,37 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
 
-//    @RequestMapping(value="")
-//    @ResponseBody
-//    public String index(@RequestParam String name) {
-//        if (name == null) {
-//            name = "World";
-//        }
-//        return "Hello " + name;
-//    }
-
-    @RequestMapping(value="hello", method = RequestMethod.GET)
-    @ResponseBody
-    public String helloForm()  {
-
-        String html = "<form method='post'>" +
-                "<input type = 'text' name = 'name' />" +
-                "<select name = 'language'>" +
-                "<option value = 'english'>English</option>" +
-                "<option value = 'french'>French</option>" +
-                "<option value = 'italian'>Italian</option>" +
-                "<option value = 'spanish'>Spanish</option>" +
-                "<option value = 'german'>German</option>" +
-                "</select>" +
-                "<input type = 'submit' value = 'Greet Me!' />" +
-                "</form>";
-        return html;
-
-        // For a bonus mission, the options of the select need to be pulled in from a model.
-        // If a student has chosen to try this mission, they need to add a models package that contains a model for the different language options.
-    }
-
-    @RequestMapping(value="hello", method = RequestMethod.POST)
-    @ResponseBody
-    public String helloPost(@RequestParam String name, @RequestParam String language) {
-        if (name == null) {
-            name = "World";
-        }
-
-        return createMessage(name, language);
-
-        // For a bonus mission, students can change this response text to look nicer.
-        // This is subjective, but students should be modifying the HTML of the response string.
-    }
-
-    public static String createMessage(String n, String l) {
-        String greeting = "";
-
-        if (l.equals("english")) {
-            greeting = "Hello";
-        }
-        else if (l.equals("french")) {
-            greeting = "Bonjour";
-        }
-        else if (l.equals("italian")) {
-            greeting = "Bonjourno";
-        }
-        else if (l.equals("spanish")) {
-            greeting = "Hola";
-        }
-        else if (l.equals("german")) {
-            greeting = "Hallo";
-        }
-
-        return greeting + " " + n;
-    }
-
-    @RequestMapping(value="hello/{name}")
-    @ResponseBody
-    public String helloUrlSegment(@PathVariable String name) {
-        return "Hello " + name;
-    }
-
-    @RequestMapping(value="goodbye")
+    @GetMapping("goodbye")
     @ResponseBody
     public String goodbye() {
-        return "Goodbye!";
+        return "Goodbye, Spring!";
     }
-}
 
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // /hello/form
+    @GetMapping("form")
+    public String helloForm() {
+        return "form";
+    }
+
+}
