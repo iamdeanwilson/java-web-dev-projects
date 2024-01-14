9d19da119467d4c20e516961f2d6f7dd8bd21921
Dean Wilson <Dean Wilson>
2024-01-14 14:57:47 -0600

    Completed Thymeleaf exercises

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,13 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = null;
+
+    @PostConstruct
+    public void init() throws Exception {
+        events = new HashMap<>();
+        {
+            events.put("Coding Event", "Meet other coders!");
+            events.put("Event for Coding", "Meet even more coders!");
+            events.put("Coding with Coders", "Meet coders who want to code!");
+        }
+    };
 
     @GetMapping
     public String displayAllEvents(Model model) {
@@ -29,8 +41,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, String eventDescription) {
+        events.put(eventName, eventDescription);
         return "redirect:/events";
     }
 
